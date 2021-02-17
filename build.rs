2c0289d64a48df015bdf3433c7ab846ba8834cb5
figsoda <figsoda@pm.me>
2021-02-17 18:48:03 -0500

    only generate completions if GEN_COMPLETIONS=1

--- build.rs
+++ build.rs
@@ -6,6 +6,12 @@ use std::{env, fs::create_dir_all, path::Path};
 include!("src/cli.rs");
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=GEN_COMPLETIONS");
+
+    if env::var_os("GEN_COMPLETIONS") != Some("1".into()) {
+        return;
+    }
+
     let out = &Path::new(&env::var_os("OUT_DIR").unwrap()).join("completions");
     create_dir_all(out).unwrap();
     let app = &mut Opts::into_app();
